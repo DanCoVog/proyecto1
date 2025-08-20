b0c6b810f851b6dc4e871117c02fb9056b393102
DanCoVog <legendaryopspvp@gmail.com>
2025-08-20 09:38:46 -0500

    agregar paguina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 // Update the import path below if the file is named differently or located elsewhere
-import Renderlistas from "./Componest/Renderlistas";
-
+import Renderlistas from "../components/renderlistas";
+import Pagina from "../components/pagina";
 
 export default function Home() {
   return (
-
-    
     <>
+      <Pagina />
       <Renderlistas />
     </>
   );
---
+++ src/components/pagina.jsx
@@ -0,0 +1,7 @@
+export default function Pagina(Props) {
+  return (
+    <div>
+      <h1>Hola soy un componente</h1>
+    </div>
+  );
+}
